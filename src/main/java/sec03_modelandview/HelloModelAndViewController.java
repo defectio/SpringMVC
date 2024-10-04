4e02b0a7a4443c532f1f2a6f8d70d01ffbc5ac46
defectio <defectio916@gmail.com>
2024-10-04 12:20:57 +0900

    sec03_modelandview

---
+++ src/main/java/sec03_modelandview/HelloModelAndViewController.java
@@ -0,0 +1,28 @@
+package sec03_modelandview;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 요청URL : http://localhost:${port}/${contextPath}/helloModelView.htm
+ * @author USER
+ */
+@Controller
+public class HelloModelAndViewController {
+
+	@RequestMapping("/helloModelView.htm")
+	public ModelAndView search() {
+		ModelAndView mav = new ModelAndView();
+		
+		//뷰페이지에서 사용할 "데이터" 저장
+		mav.addObject("name", "kim");
+		mav.addObject("age", 30);
+		
+		//"뷰페이지"에 대한 정보를 넘겨줌
+		mav.setViewName("view/sec03/helloModelView");
+		
+		return mav;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/sec03/helloModelView.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<p>ModelAndView</p>
+	
+	<!-- mav.addObject("name", "kim"); --> 
+	<!-- mav.addObject("age", 30);  -->
+	<p>name:${name }</p>
+	<p>age:${age }</p>
+</body>
+</html>
